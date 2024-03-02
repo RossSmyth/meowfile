0536ff46d0edcb87289166dd2ee809a1ac5e0a98
Ross Smyth <crs2017@gmail.com>
2024-03-01 22:20:54 -0500

    Add comments

--- crates/diagnostics/src/lib.rs
+++ crates/diagnostics/src/lib.rs
@@ -1,6 +1,12 @@
+//! Diagnostic items.
+//! To be expanded.
+
 /// A span of source code.
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Span {
+	/// zero-indexed start
 	pub start: u32,
+
+	/// zero-indexed end
 	pub end: u32,
 }
